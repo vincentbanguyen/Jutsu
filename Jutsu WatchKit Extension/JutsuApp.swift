c8ab6c9d558b3ab61e36d363cba0e76d9c7af372
Nagooy3n <vincentbanguyen@gmail.com>
2021-10-09 19:46:47 -0500

    Initial Commit

---
+++ Jutsu WatchKit Extension/JutsuApp.swift
@@ -0,0 +1,19 @@
+//
+//  JutsuApp.swift
+//  Jutsu WatchKit Extension
+//
+//  Created by Vincent Nguyen on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct JutsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ Jutsu/JutsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  JutsuApp.swift
+//  Jutsu
+//
+//  Created by Vincent Nguyen on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct JutsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
